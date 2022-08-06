e1e60719e627d2a348aab0004cb3c0fdfaee01d5
Edwin9734 <100248651+Edwin9734@users.noreply.github.com>
2022-08-06 09:35:42 -0600

    Add files via upload
    Clase con ejecutor

---
+++ Matriz.java
@@ -0,0 +1,11 @@
+package com.edwin.matriz;
+import com.edwin.dato.Clsgestion;
+
+public class Matriz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Clsgestion hu = new Clsgestion();
+        hu.menu();
+    }
+}
